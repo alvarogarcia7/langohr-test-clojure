2aa7b00f4f13f0fa3973134648f137652447134a
Alvaro Garcia <derecerca@gmail.com>
2017-04-03 09:41:57 +0200

    Remove code related to uniq-files

--- test/uniq_files/core_test.clj
+++ test/uniq_files/core_test.clj
@@ -4,8 +4,7 @@
             [langohr.queue :as lq]
             [langohr.consumers :as lc]
             [langohr.basic :as lb]
-            [uniq-files.core :refer :all]
-            [clojure.string :require :split]))
+            [uniq-files.core :refer :all]))
 
 (def ^{:const true}
 default-exchange-name "")
@@ -14,36 +13,6 @@ default-exchange-name "")
   [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
   (println (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s, type: %s"
                    (String. payload "UTF-8") delivery-tag content-type type)))
-
-(defn
-  group-by-hash
-  [lines]
-  (letfn [(tokenize
-            [line]
-            (let [[filename hash] (split line #" ")]
-              {:hash hash :filename filename}))]
-    (->>
-      lines
-      (map tokenize)
-      (reduce (fn
-                [acc ele]
-                (let [{hash :hash filename :filename} ele
-                      current (get acc hash [])]
-                  (assoc acc hash (conj current filename)))) {})
-      (map (fn [x] (print "# ") (println  x) x))
-      (map (fn
-             [[hash filenames]]
-             (let [filenames (sort filenames)]
-               (println (str "#HASH = " hash))
-               (println (str "# keep " (last filenames)))
-               (doall (map #(println "rm " %) (butlast filenames)))))))))
-
-
-(defn
-  lines
-  [path]
-  (with-open [rdr (reader path)]
-    (doall (line-seq rdr))))
 
 (defn
   connect-to-mq
